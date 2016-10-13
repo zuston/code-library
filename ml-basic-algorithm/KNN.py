c93ed175965292cdeec4da0702ce4d2e7e1f2494
zuston.shacha <zuston.shacha@gmail.com>
2016-10-13 21:01:43 +0800

    增加展示

--- ml-basic-algorithm/KNN.py
+++ ml-basic-algorithm/KNN.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 import numpy as np
+import matplotlib.pyplot as plt
 
 def knn(exmpleSet,dataset,labelset,k):
         if len(dataset)!=len(labelset):
@@ -20,6 +21,14 @@ def knn(exmpleSet,dataset,labelset,k):
         # print resCount
         return 0 if resCount[0]>resCount[1] else 1
 
+def show(exmpleSet,data,labelset,predictLabel):
+    mark = ['or', 'ob', 'og', 'ok', '^r', '+r', 'sr', 'dr', '<r', 'pr']
+    index=0
+    for td in data:
+        plt.plot(float(td[0])/10,float(td[1])/10,mark[labelset[index]],markersize=10)
+        index += 1
+    plt.plot(float(exmpleSet[0])/10,float(exmpleSet[1])/10,'pr',markersize=15)
+    plt.show()
 
 if __name__ == '__main__':
     '''
@@ -32,4 +41,6 @@ if __name__ == '__main__':
     exmpleSet = [3,2]
     dataset = [[8,3],[10,1],[45,32],[78,34],[9,18],[23,45],[34,45],[12,19]]
     labelset = [0,0,0,0,1,1,1,1]
-    print knn(exmpleSet,dataset,labelset,5)
+    predictLable =  knn(exmpleSet,dataset,labelset,5)
+    show(exmpleSet,dataset,labelset,predictLable)
+    print predictLable
